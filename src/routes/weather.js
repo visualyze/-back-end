bca0a21ece2dfef3fc6ded14396da602239acdbe
sarahduv <sarah.duvdevani@gmail.com>
2019-12-17 15:47:39 -0800

    adding afternoon tuesday updates

--- src/routes/weather.js
+++ src/routes/weather.js
@@ -1,16 +1,21 @@
-getLatLong(request, response) {
-  const { config } = this.props;
-  const urlToVisit = `https://maps.googleapis.com/maps/api/geocode/json?address=${config.city}&key=AIzaSyAkHx7LRhOcZalVqUBZPsIbJ4gisqT8yWY`;
+const superagent = require('superagent');
+
+function getLatLong(query, request, response) {
+  console.log('this is my query', query);
+  console.log('this is my request', request);
+  console.log('this is my request.body', request.body);
+  const urlToVisit = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=AIzaSyAkHx7LRhOcZalVqUBZPsIbJ4gisqT8yWY`;
   superagent
     .get(urlToVisit)
     .then(responseFromSuper => {
       const geoData = responseFromSuper.body;
-      console.log('this is geodata', geoData);
+      // console.log('this is geodata', geoData);
       const specificGeoData = geoData.results[0];
       const newLocation = [
         specificGeoData.geometry.location.lat,
         specificGeoData.geometry.location.lng
       ];
+      console.log('this is my newLocation data', newLocation);
       response.send(newLocation);
     })
     .catch(error => {
@@ -18,3 +23,5 @@ getLatLong(request, response) {
       console.error(error);
     });
 }
+
+module.exports = getLatLong;
