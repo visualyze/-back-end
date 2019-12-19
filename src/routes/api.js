a8e8607ac7d5a7ed8f0d96f84a580413f8e73271
sarahduv <sarah.duvdevani@gmail.com>
2019-12-18 20:06:31 -0800

    adding rss feed route

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const superagent = require('superagent');
 const express = require('express');
 const router = express.Router();
+require('dotenv').config();
+
+
 const {
   getDepthCorrelationMonth,
   getDepthCorrelationWeek,
@@ -15,7 +17,10 @@ const {
 
 const { getVolcanoesByRegion } = require('./volcanoes.js');
 
-// const getLatLong = require('./weather.js');
+const { getLatLong, getWeatherDailyData } = require('./weather.js');
+
+const getRssFeed = require('./rssFeed.js');
+
 
 const simpleApiRoutes = {
   '/api/volcanoesByRegion': getVolcanoesByRegion,
@@ -26,60 +31,8 @@ const simpleApiRoutes = {
   '/api/strongestEarthquakeWeek': getStrongestEarthquakeWeek,
   '/api/strongestEarthquake': getStrongestEarthquakeDay,
   '/api/hourlyEarthquakes': getHourlyEarthquakes,
-  // '/api/fetchLatLong': getLatLong,
+  '/api/rssFeed': getRssFeed,
 };
-
-function getLatLong(city, response) {
-  const urlToVisit = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyAkHx7LRhOcZalVqUBZPsIbJ4gisqT8yWY`;
-  superagent
-    .get(urlToVisit)
-    .then(responseFromSuper => {
-      const geoData = responseFromSuper.body;
-      // console.log('this is geodata', geoData);
-      const specificGeoData = geoData.results[0];
-      const newLocation = [
-        specificGeoData.geometry.location.lat,
-        specificGeoData.geometry.location.lng
-      ];
-      console.log('this is my newLocation data', newLocation);
-      response.status(200).send(newLocation);
-    })
-    .catch(error => {
-      response.status(500).send(error.message);
-      console.error(error);
-    });
-}
-
-function getWeatherHighLowData(lat, long, response) {
-  const urlToVisit = `https://api.darksky.net/forecast/46da4fc4b81fdd5a3ad2a546858b7d24/${lat},${long}`;
-  superagent
-    .get(urlToVisit)
-    .then(responseFromSuper => {
-      console.log('this is my response from super', responseFromSuper.body.daily.data);
-      response.status(200).send();
-    })
-    .catch(error => {
-      response.status(500).send(error.message);
-      console.error(error);
-    });
-}
-
-
-router.get('/api/fetchLatLong', (request, response) => {
-  getLatLong(request.query.city, response);
-});
-
-router.get('/api/fetchWeatherHighLow', (request, response) => {
-  console.log('query data', request.query);
-  let lat = Number.parseFloat(request.query.lat);
-  let long = Number.parseFloat(request.query.long);
-  console.log(lat, long);
-  getWeatherHighLowData(lat, long, response);
-  // getWeatherHighLow(request.query.latlong);
-})
-
-
-
 
 for (let routePath in simpleApiRoutes) {
   router.get(routePath, (request, response) => {
@@ -88,13 +41,26 @@ for (let routePath in simpleApiRoutes) {
       if (apiError) {
         console.log('Api error: ', apiError);
         response
-          .status(500)
-          .json(apiError)
-          .end();
+        .status(500)
+        .json(apiError)
+        .end();
         return;
       }
       response.status(200).json(jsonObj);
     });
   });
 }
+
+router.get('/api/fetchLatLong', (request, response) => {
+  getLatLong(request.query.city, response);
+});
+
+router.get('/api/fetchWeatherHighLow', (request, response) => {
+  let lat = Number.parseFloat(request.query.lat);
+  let long = Number.parseFloat(request.query.long);
+  getWeatherDailyData(lat, long, response);
+  // getWeatherHighLow(request.query.latlong);
+})
+
+
 module.exports = router;
---
+++ src/routes/rssFeed.js
@@ -0,0 +1,37 @@
+const superagent = require('superagent');
+const xmlParser = require('fast-xml-parser');
+const minuteMs = 1000 * 60;
+
+const url = 'https://news.yahoo.com/rss/mostviewed';
+
+var getRssFeedCache = {};
+const cacheKey = 'OneRss';
+const getRssFeed = (callback) => {
+  // If it's within the last minute we use our cached data
+
+  if (
+    cacheKey in getRssFeedCache &&
+    getRssFeedCache[cacheKey].time + 60000 > Date.now()
+  ) {
+    callback(null, getRssFeedCache[cacheKey].result);
+    return;
+  }
+
+  superagent.get(url).end((apiError, apiResponse) => {
+    if (apiError) {
+      callback(apiError, null);
+      return;
+    }
+
+    const xmlBody = apiResponse.body.toString();
+    const jsonObj = xmlParser.parse(xmlBody);
+    // Update cache data
+    getRssFeedCache[cacheKey] = {};
+    getRssFeedCache[cacheKey].result = jsonObj.rss.channel.item;
+    getRssFeedCache[cacheKey].time = Date.now;
+    console.log('rss feed', jsonObj);
+    callback(null, getRssFeedCache[cacheKey].result);
+  });
+};
+
+module.exports = getRssFeed;
